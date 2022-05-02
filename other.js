9abbbeda67321307a76419e4f75adf3a7d0e5f6d
Matthew McGregor <matthew@22apps.com>
2022-05-02 14:20:31 -0500

    comments

--- other.js
+++ other.js
@@ -1,3 +1,4 @@
+// NOT NEEDED ANYMORE!!
 //if (document.getElementById("submitbutton")!==null){
     //document.getElementById("submitbutton").addEventListener("click",function(){
      //   document.getElementById("load1").style.display==="block"
